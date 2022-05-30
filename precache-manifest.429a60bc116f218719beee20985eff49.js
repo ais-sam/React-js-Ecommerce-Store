ff4f242dd4542bf9a224f27bc04c0ec5952375b4
aissam <aissam.16.25@gmail.com>
2022-05-30 12:06:47 +0100

    Updates

---
+++ precache-manifest.429a60bc116f218719beee20985eff49.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63af29c59017a1ba452185808fdc6695",
+    "url": "/React-js-Ecommerce-Store/index.html"
+  },
+  {
+    "revision": "d14595a9e3bae7914af1",
+    "url": "/React-js-Ecommerce-Store/static/css/main.bfbd1758.chunk.css"
+  },
+  {
+    "revision": "036939b6daeb93531112",
+    "url": "/React-js-Ecommerce-Store/static/js/2.1ee7aa77.chunk.js"
+  },
+  {
+    "revision": "51fe97e89231493f9eac67ba37259b61",
+    "url": "/React-js-Ecommerce-Store/static/js/2.1ee7aa77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d14595a9e3bae7914af1",
+    "url": "/React-js-Ecommerce-Store/static/js/main.c58842af.chunk.js"
+  },
+  {
+    "revision": "ef8d515d598d5e53f6ef",
+    "url": "/React-js-Ecommerce-Store/static/js/runtime-main.d56cc5a2.js"
+  },
+  {
+    "revision": "d0c54d11ea3841298f0c0cecfb6a0271",
+    "url": "/React-js-Ecommerce-Store/static/media/arrow.d0c54d11.svg"
+  },
+  {
+    "revision": "4dd5809e3ddc5436ef5daccb5355648a",
+    "url": "/React-js-Ecommerce-Store/static/media/cart.4dd5809e.svg"
+  },
+  {
+    "revision": "176425bf0b53c249d9d495d02ff57126",
+    "url": "/React-js-Ecommerce-Store/static/media/hero.176425bf.png"
+  },
+  {
+    "revision": "fa1e58b79d668a9a579c0987f2a3d03c",
+    "url": "/React-js-Ecommerce-Store/static/media/item-1.fa1e58b7.png"
+  },
+  {
+    "revision": "1a17e4b5008ee876a4217c38c3ba37f5",
+    "url": "/React-js-Ecommerce-Store/static/media/item-2.1a17e4b5.png"
+  },
+  {
+    "revision": "90ed35252447e6f24909c14a68d9f93b",
+    "url": "/React-js-Ecommerce-Store/static/media/item-3.90ed3525.png"
+  },
+  {
+    "revision": "4f7d42dfa1c7cadc132b3970b88f8f82",
+    "url": "/React-js-Ecommerce-Store/static/media/item-4.4f7d42df.png"
+  },
+  {
+    "revision": "1b42bb5e132c6063f6eb2013948d986a",
+    "url": "/React-js-Ecommerce-Store/static/media/item-5.1b42bb5e.png"
+  },
+  {
+    "revision": "ecde975e03be2a9b21aab48d919ee735",
+    "url": "/React-js-Ecommerce-Store/static/media/item-6.ecde975e.png"
+  },
+  {
+    "revision": "dce114dec10fe97fa709424671473f2e",
+    "url": "/React-js-Ecommerce-Store/static/media/item-7.dce114de.png"
+  },
+  {
+    "revision": "cc8474d12ddc8e052f81fc6b317667ab",
+    "url": "/React-js-Ecommerce-Store/static/media/item-8.cc8474d1.png"
+  },
+  {
+    "revision": "5d61c534594b2e2ba3c3d6c560cbcb15",
+    "url": "/React-js-Ecommerce-Store/static/media/item-9.5d61c534.png"
+  },
+  {
+    "revision": "0f27d0b1be39f848d4b97b06644b519e",
+    "url": "/React-js-Ecommerce-Store/static/media/logo.0f27d0b1.svg"
+  },
+  {
+    "revision": "6a97656714be3fad78c7b4a7167b2c46",
+    "url": "/React-js-Ecommerce-Store/static/media/mailing-list-img.6a976567.png"
+  }
+]);
